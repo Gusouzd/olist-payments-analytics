6769750b75ed5cffc874bf91726dabf1820dce26
Gustavo Souza <gurafael@hotmail.com.br>
2026-08-05 11:49:29 -0300

    Adiciona a ligação com o dbt no BigQuery

--- src/extractor.py
+++ src/extractor.py
@@ -10,14 +10,22 @@ def extrair_tabelas(caminho):
     for arquivo in pasta.iterdir():
         nome = arquivo.stem
         nome = nome.replace('olist_', '').replace('_dataset', '')
+        logging.info(f"Lendo {nome}...")
         dataframes[nome] = pd.read_csv(arquivo)
+    logging.info("Arquivos lidos com sucesso.")
 
+    logging.info("Validando os dados lidos...")
     for nome, df in dataframes.items():
+        logging.info(f"{nome} — {len(df)} linhas lidas")
         if df.empty:
             raise ValueError(f"Tabela {nome} chegou vazia")
-        
+    
         for coluna in df.columns:
             if ('date' in coluna) or ('timestamp' in coluna):
                 df[coluna] = pd.to_datetime(df[coluna], errors='coerce')
+    
+    logging.info(f"{nome} — tipos convertidos")
+
+    logging.info("Validação concluída com sucesso.")
 
     return dataframes
--- src/main.py
+++ src/main.py
@@ -1,13 +1,24 @@
 from extractor import extrair_tabelas
 from loader import carregar_tabelas
 from dotenv import load_dotenv
+import logging
 import os
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('pipeline.log'),
+        logging.StreamHandler()
+    ]
+)
 
 load_dotenv()
 
 caminho = os.environ["DATA_PATH"]
 
 dataframes = extrair_tabelas(caminho)
+
 
 for nome, df in dataframes.items():
     carregar_tabelas(nome, df)
